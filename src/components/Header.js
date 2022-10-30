f0e43e3d5287fb8bf49fb8c6c8e0e67d1471c982
lucia <ruadaslucia@gmail.com>
2022-10-30 20:33:17 +0100

    responsive done

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,12 +1,12 @@
 import "../styles/layouts/header.scss";
-import Menu from "./Menu";
+// import Menu from "./Menu";
 
 function Header() {  
 
     return (
       <div className="header">
       <h1 className='headerTitle'>portfolio ruadaslucia</h1>
-      <Menu/>
+      {/* <Menu/> */}
       </div>
 
       
--- src/components/SectionRrss.js
+++ src/components/SectionRrss.js
@@ -2,7 +2,7 @@ import "../styles/layouts/sectionRrss.scss";
 function SectionRrss() {
     return (
       <div className="sectionRrss">
-        <h3 className='sectionRrssTitle'>Redes sociales</h3>
+        <h3 className='sectionRrssTitle'>RR.SS.</h3>
         <h5 className='sectionRrssText'>You can find me here or write me directly on my <a href="mailto: ruadaslucia@gmail.com" className='sectionRrssEmail'> email</a>.</h5>
         <ul className='sectionRrssList'>
           <li className='sectionRrssItem'><a href="https://github.com/ruadaslucia">Github</a></li>

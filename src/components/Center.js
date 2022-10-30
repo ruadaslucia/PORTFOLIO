1b16283ad407f545e274e5e46431c6235330b370
lucia <ruadaslucia@gmail.com>
2022-10-30 18:15:58 +0100

    responsive done

--- src/components/Center.js
+++ src/components/Center.js
@@ -1,17 +1,18 @@
 import "../styles/layouts/center.scss";
-// import SectionMe from './SectionMe';
-//import SectionSkills from './SectionSkills';
+import SectionMe from './SectionMe';
+import SectionSkills from './SectionSkills';
 import SectionProjects from "./SectionProjects";
-
+import SectionRrss from "./SectionRrss";
 
 function Center() {
     return (
       <main className="center">
         <div className="sections">
           
-          {/* <SectionMe/> */}
-          {/* <SectionSkills/>  */}
-          <SectionProjects/>
+          <SectionMe/>
+          <SectionSkills/> 
+          <SectionProjects/> 
+          <SectionRrss/>
 
         </div>
       </main>
--- src/components/SectionRrss.js
+++ src/components/SectionRrss.js
@@ -1,7 +1,15 @@
 import "../styles/layouts/sectionRrss.scss";
 function SectionRrss() {
     return (
-      <div className="sectionRrss">SectionRrss sectionRrss
+      <div className="sectionRrss">
+        <h3 className='sectionRrssTitle'>Redes sociales</h3>
+        <h5 className='sectionRrssText'>You can find me here or write me directly on my <a href="mailto: ruadaslucia@gmail.com" className='sectionRrssEmail'> email</a>.</h5>
+        <ul className='sectionRrssList'>
+          <li className='sectionRrssItem'><a href="https://github.com/ruadaslucia">Github</a></li>
+          <li className='sectionRrssItem'><a href="https://www.linkedin.com/in/ruadaslucia/">Linkedin</a></li>
+          <li className='sectionRrssItem'><a href="https://twitter.com/ruadasLucia">Twitter</a></li>
+        </ul>
+        {/* <a href="mailto: ruadaslucia@gmail.com">My email</a> */}
       </div>
     );
   }

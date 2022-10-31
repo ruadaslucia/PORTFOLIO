29a1ef297ad8e06584510ea6333c3ad4c69c9d9e
lucia <ruadaslucia@gmail.com>
2022-10-31 10:06:37 +0100

    icons and favicon

--- src/components/SectionRrss.js
+++ src/components/SectionRrss.js
@@ -3,11 +3,11 @@ function SectionRrss() {
     return (
       <div className="sectionRrss">
         <h3 className='sectionRrssTitle'>RR.SS.</h3>
-        <h5 className='sectionRrssText'>You can find me here or write me directly on my <a href="mailto: ruadaslucia@gmail.com" className='sectionRrssEmail'> email</a>.</h5>
+        <h5 className='sectionRrssText'>You can find me here or write me directly on my <a href="mailto: ruadaslucia@gmail.com" className='sectionRrssEmail'> email</a></h5>
         <ul className='sectionRrssList'>
-          <li className='sectionRrssItem'><a href="https://github.com/ruadaslucia">Github</a></li>
-          <li className='sectionRrssItem'><a href="https://www.linkedin.com/in/ruadaslucia/">Linkedin</a></li>
-          <li className='sectionRrssItem'><a href="https://twitter.com/ruadasLucia">Twitter</a></li>
+          <li className='sectionRrssItem'><a href="https://github.com/ruadaslucia"><i class="fa-brands fa-github"></i></a></li>
+          <li className='sectionRrssItem'><a href="https://www.linkedin.com/in/ruadaslucia/"><i class="fa-brands fa-linkedin"></i></a></li>
+          <li className='sectionRrssItem'><a href="https://twitter.com/ruadasLucia"><i class="fa-brands fa-twitter"></i></a></li>
         </ul>
       </div>
     );

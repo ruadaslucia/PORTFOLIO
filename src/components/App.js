4ba9a8f504ac9bfd9e5c907f0fd3aa2d2aee1999
lucia <ruadaslucia@gmail.com>
2022-11-10 11:28:38 +0100

    portfolio

--- src/components/App.js
+++ src/components/App.js
@@ -1,28 +1,25 @@
-import'../styles/main.scss';
-import Header from "./Header";
+import '../styles/main.scss';
+import Header from './Header';
 import SectionMe from './SectionMe';
 import SectionSkills from './SectionSkills';
-import SectionProjects from "./SectionProjects";
-import SectionRrss from "./SectionRrss";
-import Footer from "./Footer";
+import SectionProjects from './SectionProjects';
+import SectionRrss from './SectionRrss';
+import Footer from './Footer';
 import { Route, Routes } from 'react-router-dom';
-
-
 
 function App() {
   return (
     <>
-    <Header/>
-    <Routes>
-        <Route path="/" element={<SectionMe/>} />
-        <Route path="/skills"element={<SectionSkills/>}/>
-        <Route path="/projects"element={<SectionProjects/>}/>
-        <Route path="/rrss"element={<SectionRrss/>}/>        
-    </Routes>
-    <Footer/>  
+      <Header />
+      <Routes>
+        <Route path="/" element={<SectionMe />} />
+        <Route path="/skills" element={<SectionSkills />} />
+        <Route path="/projects" element={<SectionProjects />} />
+        <Route path="/rrss" element={<SectionRrss />} />
+      </Routes>
+      <Footer />
     </>
   );
 }
 
 export default App;
-

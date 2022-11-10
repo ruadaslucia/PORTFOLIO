7b0b64575ae0f3cdebd10c196de09e438bed23c3
lucia <ruadaslucia@gmail.com>
2022-11-10 18:35:25 +0100

    change route me

--- src/components/Menu.js
+++ src/components/Menu.js
@@ -1,47 +1,42 @@
 import { Link } from 'react-router-dom';
 
 function Menu() {
-  
-
-    return (
-     <div className='menu'>
+  return (
+    <div className="menu">
       <ul className="menuList">
-        <li className='menuList__listItem'>          
-          <button className="menuList__listItem--listButton">             
-            <Link className="menuList__listItem--link" to="/">
-            me
+        <li className="menuList__listItem">
+          <button className="menuList__listItem--listButton">
+            <Link className="menuList__listItem--link" to="/PORTFOLIO">
+              me
             </Link>
           </button>
-          
         </li>
 
-        <li className='menuList__listItem'>          
+        <li className="menuList__listItem">
           <button className="menuList__listItem--listButton">
-          <Link className="menuList__listItem--link" to="/skills">
+            <Link className="menuList__listItem--link" to="/skills">
               skills
             </Link>
           </button>
         </li>
 
-        <li className='menuList__listItem'>          
+        <li className="menuList__listItem">
           <button className="menuList__listItem--listButton">
-          <Link className="menuList__listItem--link" to="/projects">
-          projects
-            </Link>
-            
-          </button>
-        </li>
-
-        <li className='menuList__listItem'>          
-          <button className="menuList__listItem--listButton">
-          <Link className="menuList__listItem--link" to="/rrss">
-          rrss
+            <Link className="menuList__listItem--link" to="/projects">
+              projects
             </Link>
           </button>
         </li>
 
+        <li className="menuList__listItem">
+          <button className="menuList__listItem--listButton">
+            <Link className="menuList__listItem--link" to="/rrss">
+              rrss
+            </Link>
+          </button>
+        </li>
       </ul>
-      </div>
-    );
-  }
-  export default Menu;
+    </div>
+  );
+}
+export default Menu;

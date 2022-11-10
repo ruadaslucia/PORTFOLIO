9a17f7661c0662fe3226547dc374053e2ec6ab47
lucia <ruadaslucia@gmail.com>
2022-11-10 11:54:17 +0100

    portfolio/

--- src/components/App.js
+++ src/components/App.js
@@ -12,7 +12,7 @@ function App() {
     <>
       <Header />
       <Routes>
-        <Route path="/" element={<SectionMe />} />
+        <Route path="/PORTFOLIO" element={<SectionMe />} />
         <Route path="/skills" element={<SectionSkills />} />
         <Route path="/projects" element={<SectionProjects />} />
         <Route path="/rrss" element={<SectionRrss />} />
